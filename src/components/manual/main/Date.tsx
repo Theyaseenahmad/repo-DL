f019cd569cb46983853cb1172617e840a0051f21
Yaseen Ahmad <yaseenahmad02.777@gmail.com>
2025-03-07 23:40:30 +0530

    change positioning of date

--- src/components/manual/main/Date.tsx
+++ src/components/manual/main/Date.tsx
@@ -15,7 +15,7 @@ export function CalendarForm() {
 
   return (
     <button
-    className="inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-md text-[10px] sm:text-xs ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-3 [&_svg]:shrink-0 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-8 px-2 w-auto max-w-[160px] pl-2 text-left font-normal text-muted-foreground mx-auto md:ml-auto md:mr-0"
+    className="inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-md text-[10px] sm:text-xs ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-3 [&_svg]:shrink-0 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-8 px-2 w-auto max-w-[160px] pl-2 text-left font-normal text-muted-foreground  md:ml-auto md:mr-0 mr-0"
     id=":r3f:-form-item"
     aria-describedby=":r3f:-form-item-description"
     aria-invalid="false"
